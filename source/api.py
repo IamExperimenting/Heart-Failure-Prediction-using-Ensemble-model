3af87e465803eebddfe25af8582a783ffabc0b10
Iamexperimenting <iamexperimentingnow@gmail.com>
2021-03-21 19:44:16 +0000

    adding pytest for incorrect url, data and header

--- source/api.py
+++ source/api.py
@@ -3,14 +3,64 @@ from fastapi import FastAPI,Header, HTTPException
 from variables import Features
 from fastapi.testclient import TestClient
 from configparser import ConfigParser
+from pydantic import BaseModel
 
 parser = ConfigParser()
 parser.read('config.ini')
+
+secret_token = "fastapitest"
+
+dummy_json = {
+  "age": 75,
+  "anaemia": 0,
+  "creatinine_phosphokinase": 582,
+  "diabetes": 0,
+  "ejection_fraction": 20,
+  "high_blood_pressure": 1,
+  "platelets": 265000.00,
+  "serum_creatinine": 1.9,
+  "serum_sodium": 130,
+  "sex": 1,
+  "smoking": 0,
+  "time": 4
+}
 
 model_output_path = parser.get('output_path','pickle_file')
 ensemble_model = joblib.load(model_output_path)
 
 app = FastAPI()
+
+class Features(BaseModel):
+    age : float
+    anaemia : int
+    creatinine_phosphokinase : int
+    diabetes : int
+    ejection_fraction : int
+    high_blood_pressure : int
+    platelets : float
+    serum_creatinine : float
+    serum_sodium : int
+    sex : int
+    smoking : int
+    time : int
+
+@app.get("/test/{item_id}", response_model=Features)
+async def read_main(item_id: str, x_token: str = Header(...)):
+    if x_token != secret_token:
+        raise HTTPException(status_code=400, detail="Invalid X-Token header")
+    if item_id not in dummy_json:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return dummy_json[item_id]
+
+
+@app.post("/test/", response_model=Features)
+async def create_item(item: Features, x_token: str = Header(...)):
+    if x_token != secret_token:
+        raise HTTPException(status_code=400, detail="Invalid X-Token header")
+    if item.age in dummy_json:
+        raise HTTPException(status_code=400, detail="Item already exists")
+    dummy_json[item.age] = item
+    return item
 
 @app.get('/')
 async def index():
--- source/api_test.py
+++ source/api_test.py
@@ -1,11 +1,100 @@
 from fastapi.testclient import TestClient
-from app import app
+
+from api import app
+
 client = TestClient(app)
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"msg": "Assignment Page"}
-    print('Testing done..........')
 
-test_read_main()
+
+def test_read_item_bad_token():
+    response = client.get("/test/bad_data", headers={"X-Token": "hailhydra"})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Invalid X-Token header"}
+
+
+def test_read_inexistent_item():
+    response = client.get("/test/notin", headers={"X-Token": "fastapitest"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Item not found"}
+
+
+def test_create_item():
+    response = client.post(
+        "/test/",
+        headers={"X-Token": "fastapitest"},
+        json={
+  "age": 75,
+  "anaemia": 0,
+  "creatinine_phosphokinase": 582,
+  "diabetes": 0,
+  "ejection_fraction": 20,
+  "high_blood_pressure": 1,
+  "platelets": 265000.00,
+  "serum_creatinine": 1.9,
+  "serum_sodium": 130,
+  "sex": 1,
+  "smoking": 0,
+  "time": 4
+},
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+  "age": 75,
+  "anaemia": 0,
+  "creatinine_phosphokinase": 582,
+  "diabetes": 0,
+  "ejection_fraction": 20,
+  "high_blood_pressure": 1,
+  "platelets": 265000.00,
+  "serum_creatinine": 1.9,
+  "serum_sodium": 130,
+  "sex": 1,
+  "smoking": 0,
+  "time": 4
+}
+
+
+def test_create_item_bad_token():
+    response = client.post(
+        "/test/",
+        headers={"X-Token": "hailhydra"},
+        json={
+  "age": 75,
+  "anaemia": 0,
+  "creatinine_phosphokinase": 582,
+  "diabetes": 0,
+  "ejection_fraction": 20,
+  "high_blood_pressure": 1,
+  "platelets": 265000.00,
+  "serum_creatinine": 1.9,
+  "serum_sodium": 130,
+  "sex": 1,
+  "smoking": 0,
+  "time": 4
+},
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Invalid X-Token header"}
+
+
+def test_create_existing_item():
+    response = client.post(
+        "/test/",
+        headers={"X-Token": "fastapitest"},
+        json={
+  "age": 75,
+  "anaemia": 0,
+  "creatinine_phosphokinase": 582,
+  "diabetes": 0,
+  "ejection_fraction": 20,
+  "high_blood_pressure": 1,
+  "platelets": 265000.00,
+  "serum_creatinine": 1.9,
+  "serum_sodium": 130,
+  "sex": 1,
+  "smoking": 0,
+  "time": 4
+},
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Item already exists"}
